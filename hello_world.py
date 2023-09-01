145b931ae8c965cea9d264d71fe89e50f1d13a33
ivan-pua-tpg <ivan-pua-tpg>
2023-09-01 03:10:18 +0000

    first commit from dbx

---
+++ hello_world.py
@@ -0,0 +1,14 @@
+# Databricks notebook source
+print("Hello world!")
+
+# COMMAND ----------
+
+# MAGIC %sql
+# MAGIC
+# MAGIC select *
+# MAGIC from dev.ivan_dev.all_days
+# MAGIC limit 10
+
+# COMMAND ----------
+
+
